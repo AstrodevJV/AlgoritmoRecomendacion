665865a8d9c2294622454b9626343361f8e68d20
DiegoGCV <dc2873199@gmail.com>
2025-03-06 13:17:34 -0500

    Final design

--- scripts/prediccionscript.js
+++ scripts/prediccionscript.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const resultadoCarta = document.getElementById("resultadoCarta");
 
-
     const categoriaRecomendada = localStorage.getItem("categoriaRecomendada");
     const imagenesRonda10 = JSON.parse(localStorage.getItem("imagenesRonda10")) || [];
 
@@ -10,15 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
     }
 
-
     const imagenRecomendada = imagenesRonda10.find(img => img.categoria === categoriaRecomendada);
 
     if (imagenRecomendada) {
         resultadoCarta.innerHTML = `
             <img src="${imagenRecomendada.url}" alt="Recomendación">
-            <p>${categoriaRecomendada}</p>
+            <p>Tu última elección pudo ser</p>
         `;
     } else {
         resultadoCarta.innerHTML = `<p>No se encontró una imagen que coincida con la categoría recomendada: ${categoriaRecomendada}.</p>`;
     }
+
+    var h1 = document.getElementById("principal");
+    h1.innerHTML = `Creemos que te gusta: ${categoriaRecomendada}`;
+
 });
