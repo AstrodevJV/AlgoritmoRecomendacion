d5aedb6423d4946953f528492577797ddeea4d49
AstrodevJV <soalejandro@protonmail.com>
2025-03-06 08:17:58 -0500

    Cambion en el archivo de aleatoriedad

--- scripts/mostrarCartasAleatorias.js
+++ scripts/mostrarCartasAleatorias.js
@@ -48,27 +48,6 @@ const images = [
 
 ];
 
-function getUniqueCategoryCards() {
-    const categoryMap = {};
-    images.forEach(img => {
-        if (!categoryMap[img.category]) {
-            categoryMap[img.category] = [];
-        }
-        categoryMap[img.category].push(img);
-    });
-
-    const uniqueCards = [];
-    const categories = Object.keys(categoryMap).sort(() => Math.random() - 0.5);
-
-    for (let i = 0; i < categories.length && uniqueCards.length < 4; i++) {
-        const categoryImages = categoryMap[categories[i]];
-        const randomImage = categoryImages[Math.floor(Math.random() * categoryImages.length)];
-        uniqueCards.push(randomImage);
-    }
-
-    return uniqueCards;
-}
-
 function displayCards() {
     const cardContainer = document.getElementById('cardContainer');
     const selectedCards = getUniqueCategoryCards();
@@ -81,7 +60,8 @@ function displayCards() {
         card.dataset.category = imageObj.category;
 
         const img = document.createElement('img');
-        img.src = imageObj.src;
+        
+        img.src = imageObj.src + "?t=" + new Date().getTime();
         img.alt = 'Carta';
 
         const categoryLabel = document.createElement('p');
@@ -99,12 +79,3 @@ function displayCards() {
         });
     });
 }
-
-document.getElementById('confirmSelection').addEventListener('click', () => {
-    const selectedCards = document.querySelectorAll('.card[data-selected="true"]');
-    const selectedCategories = new Set();
-    selectedCards.forEach(card => selectedCategories.add(card.dataset.category));
-    document.getElementById('selectedCategories').textContent = "Categorías seleccionadas: " + Array.from(selectedCategories).join(', ');
-});
-
-displayCards();
