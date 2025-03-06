089221813ba2fb434ff83354b57c80557f045fe6
AstrodevJV <soalejandro@protonmail.com>
2025-03-05 19:56:48 -0500

    pequeños cambios en el archivo inicio

--- scripts/inicio.js
+++ scripts/inicio.js
@@ -1,2 +1,10 @@
 let clave = "Papu alianza";
 
+let input = document.getElementById("inputClave");
+let button = document.getElementById("initialButton");
+
+button.addEventListener("click", function() {
+    if (input.value === clave) {
+        
+    }
+})
